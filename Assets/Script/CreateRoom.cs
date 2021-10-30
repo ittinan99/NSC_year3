8f68a3c0e830c29efc0f06a02bf787847990e9a7
Miyasaki-Kei <theerawat.kei@mail.kmutt.ac.th>
2021-10-30 15:00:00 +0700

    MenuSceneManager#1

--- Assets/Script/CreateRoom.cs
+++ Assets/Script/CreateRoom.cs
@@ -7,47 +7,59 @@ using Unity.Netcode;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 using UnityEngine.SceneManagement;
-
-namespace Netcode.Transports.PhotonRealtime
+using TMPro;
+using Netcode.Transports.PhotonRealtime;
+public class CreateRoom : MonoBehaviour
 {
-    public class CreateRoom : MonoBehaviour
+    public string roomName = "Test";
+    public string nickName = "TestName";
+    public string sceneName;
+    public TMP_Text[] RoomID;
+    public TMP_Text[] RoomPW;
+    PhotonRealtimeTransport transport;
+    GameObject[] NetworkManagers;
+    // Happen on server
+    public void Start()
     {
-        public string roomName = "Test";
-        public string nickName = "TestName";
-        public string sceneName;
-        PhotonRealtimeTransport transport;
-        // Happen on server
-        public void Host()
+        NetworkManagers = GameObject.FindGameObjectsWithTag("NetworkManager");
+        if(NetworkManagers.Length > 1)
         {
-            NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
-            NetworkManager.Singleton.StartHost();
-            NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-        }
-        // Happen on server
-        private void ApprovalCheck(byte[] connectionData, ulong clientID, NetworkManager.ConnectionApprovedDelegate callback)
-        {
-            //Check incoming data
-            bool approve = System.Text.Encoding.ASCII.GetString(connectionData) == "Password1234";
-            callback(true, null, approve, GetRandomSpawn(), Quaternion.identity);
-        }
-        public void Join()
-        {
-            transport = NetworkManager.Singleton.GetComponent<PhotonRealtimeTransport>();
-            transport.RoomName = roomName;
-            transport.NickName = nickName;
-            NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("Password1234");
-            NetworkManager.Singleton.StartClient();
-        }
-        Vector3 GetRandomSpawn()
-        {
-            float x = UnityEngine.Random.Range(-10f, 10f);
-            float y = 4f;
-            float z = UnityEngine.Random.Range(-10f, 10f);
-            return new Vector3(x, y, z);
-        }
-        public void RoomNameChanged(string newRoomName)
-        {
-            this.roomName = newRoomName;
+            Destroy(NetworkManagers[1]);
         }
     }
+    public void Host()
+    {
+        GameObject.Find("NetworkManager").GetComponent<PhotonRealtimeTransport>().RoomName = RoomID[0].text;
+        GameObject.Find("NetworkManager").GetComponent<PhotonRealtimeTransport>().NickName = RoomID[0].text;
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+    // Happen on server
+    private void ApprovalCheck(byte[] connectionData, ulong clientID, NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        //Check incoming data
+        bool approve = System.Text.Encoding.ASCII.GetString(connectionData) == RoomPW[0].text;
+        callback(true, null, approve, GetRandomSpawn(), Quaternion.identity);
+    }
+    public void Join()
+    {
+        transport = NetworkManager.Singleton.GetComponent<PhotonRealtimeTransport>();
+        transport.RoomName = RoomID[1].text;
+        transport.NickName = nickName;
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes(RoomPW[1].text);
+        NetworkManager.Singleton.StartClient();
+    }
+    Vector3 GetRandomSpawn()
+    {
+        float x = UnityEngine.Random.Range(-10f, 10f);
+        float y = 4f;
+        float z = UnityEngine.Random.Range(-10f, 10f);
+        return new Vector3(x, y, z);
+    }
+    public void RoomNameChanged(string newRoomName)
+    {
+        this.roomName = newRoomName;
+    }
 }
+
