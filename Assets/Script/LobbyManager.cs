01646c4d833759ab8d7f43b5a01f0b6808a4da6b
Miyasaki-Kei <theerawat.kei@mail.kmutt.ac.th>
2021-11-09 01:16:13 +0700

    JointLobby#2

--- Assets/Script/LobbyManager.cs
+++ Assets/Script/LobbyManager.cs
@@ -9,14 +9,23 @@ public class LobbyManager : NetworkBehaviour
     PhotonRealtimeTransport photo;
     private string name;
     private TMP_Text NameText;
-    NetworkObject[] Player = new NetworkObject[4];
+    GameObject[] Player = new GameObject[4];
+    public GameObject Prefab;
+    public Transform Panel;
+    int count = 0;
+    int playercount = 0;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        Player[0] = GameObject.FindObjectOfType<NetworkObject>();
-        if (Player[0].IsLocalPlayer)
+        if (GameObject.Find("NetworkManager").GetComponent<NetworkManager>().IsServer && count == 0)
         {
-            PlayerChangeNameServerRpc();
+            count++;
+            playercount = GameObject.FindGameObjectsWithTag("NetworkObject").Length;
+            PlayerSpawn();
+            if (Player[playercount].GetComponent<NetworkObject>().IsLocalPlayer)
+            {
+                PlayerChangeNameServerRpc();
+            }
         }
     }
     [ServerRpc]
@@ -27,9 +36,15 @@ public class LobbyManager : NetworkBehaviour
     [ClientRpc]
     void PlayerChangeNameClientRpc()
     {
-        NameText = GameObject.Find("PlayerInfoBase").GetComponent<TMP_Text>();
+        NameText = GameObject.Find("PlayerInfoText (TMP)").GetComponent<TMP_Text>();
         photo = GameObject.Find("NetworkManager").GetComponent<PhotonRealtimeTransport>();
         name = photo.NickName;
         NameText.text = name;
+    }
+    void PlayerSpawn()
+    {
+        Player[playercount] = Instantiate(Prefab, Panel);
+        Player[playercount].GetComponent<RectTransform>().sizeDelta = new Vector2(320, 600);
+        Player[playercount].GetComponent<NetworkObject>().Spawn();
     }
 }
