bdb01c43711e3562a4396b7377f5d79fbaad315c
Miyasaki-Kei <theerawat.kei@mail.kmutt.ac.th>
2021-11-08 21:44:56 +0700

    MainMenuManager#2

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
