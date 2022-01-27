6441d00b458d094720f911ae8252b8f3d61a2621
ittinan99 <61344879+ittinan99@users.noreply.github.com>
2022-01-27 12:06:45 +0700

    fix_give_kei

--- Assets/Model/Asset/PlayerController.cs
+++ Assets/Model/Asset/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : NetworkBehaviour
     private NetworkVariable<PlayerAnimeState> networkPlayerState = new NetworkVariable<PlayerAnimeState>();
 
     [SerializeField]
-    private NetworkVariable<float> networkAimLayerWeight = new NetworkVariable<float>();
+    private NetworkVariable<float> networkAimLayerWeight = new NetworkVariable<float>(NetworkVariableReadPermission.Everyone);
 
     [SerializeField]
     private NetworkVariable<bool> networkPlayerAim = new NetworkVariable<bool>();
@@ -46,6 +46,7 @@ public class PlayerController : NetworkBehaviour
 
     private Vector3 oldInputPosition;
     private Vector3 oldInputRotation;
+    private float oldAimLayerWeight;
 
     Animator animator;
 
@@ -55,7 +56,7 @@ public class PlayerController : NetworkBehaviour
     [SerializeField]
     private int hurtLayer;
 
-    private bool playerAim = true;
+    private bool playerAim = false;
     private bool playerHurt = false;
     private bool playerThrow = false;
 
@@ -66,42 +67,79 @@ public class PlayerController : NetworkBehaviour
     public GameObject camZoomOut;
     public GameObject camZoomIn;
 
-    private void PlayerNotAim()
+    public Transform cameraTransform = null;
+    float pitch = 0f;
+    [Range(1f, 90f)]
+    public float maxPitch = 85f;
+    [Range(-1f, -90f)]
+    public float minPitch = -85f;
+    [Range(0.5f, 5f)]
+    public float mouseSensitivity = 2f;
+    public GameObject FB;
+
+    [ServerRpc]
+    private void PlayerAimServerRpc(float weight)
+    {
+        PlayerAimClientRpc(weight);
+    }
+    [ClientRpc]
+    private void PlayerAimClientRpc(float weight)
     {
         networkAimLayerWeight.Value = animator.GetLayerWeight(aimLayer);
-        animator.SetLayerWeight(aimLayer, Mathf.SmoothDamp(networkAimLayerWeight.Value, 0f, ref layerWeightVelocity, 0.2f));
+        animator.SetLayerWeight(aimLayer, Mathf.SmoothDamp(networkAimLayerWeight.Value, weight, ref layerWeightVelocity, 0.2f));
 
-        speed = 10.0F;
-        camZoomOut.SetActive(true);
-        camZoomIn.SetActive(false);
-    }
-    private void PlayerAim()
-    {
-        networkAimLayerWeight.Value = animator.GetLayerWeight(aimLayer);
-        animator.SetLayerWeight(aimLayer, Mathf.SmoothDamp(networkAimLayerWeight.Value, 1f, ref layerWeightVelocity, 0.2f));
-
-        speed = 2.0F;
-        camZoomOut.SetActive(false);
-        camZoomIn.SetActive(true);
-    }
-    private void PlayerNotHurt()
-    {
-        animator.SetBool("Hurt", false);
-    }
-    private void PlayerHurt()
-    {
-        animator.SetBool("Hurt", true);
+        if(weight != 1)
+        { 
+            if (IsLocalPlayer)
+            {
+                speed = 10.0F;
+                camZoomOut.SetActive(true);
+                camZoomIn.SetActive(false);
+            }
+        }
+        else if(weight == 1)
+        {
+            if (IsLocalPlayer)
+            {
+                speed = 2.0F;
+                camZoomOut.SetActive(false);
+                camZoomIn.SetActive(true);
+            }
+        }
     }
 
-    private void PlayerNotThrow()
-    {
-        animator.SetBool("Throw", false);
-    }
-    private void PlayerThrow()
-    {
-        animator.SetBool("Throw", true);
-    }
+    //private void PlayerNotHurt()
+    //{
+    //    animator.SetBool("Hurt", false);
+    //}
+    //private void PlayerHurt()
+    //{
+    //    animator.SetBool("Hurt", true);
+    //}
 
+    //private void PlayerNotThrow()
+    //{
+    //    animator.SetBool("Throw", false);
+    //}
+    //private void PlayerThrow()
+    //{
+    //    animator.SetBool("Throw", true);
+    //}
+    void Look()
+    {
+        cameraTransform = GetComponentInChildren<Camera>().transform;
+        //get the mouse inpuit axis values
+        float xInput = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float yInput = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        //turn the whole object based on the x input
+        transform.Rotate(0, xInput, 0);
+        //now add on y input to pitch, and clamp it
+        pitch -= yInput;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        //create the local rotation value for the camera and set it
+        Quaternion rot = Quaternion.Euler(pitch, 0, 0);
+        cameraTransform.localRotation = rot;
+    }
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -109,10 +147,18 @@ public class PlayerController : NetworkBehaviour
         aimLayer = animator.GetLayerIndex("Aiming");
         hurtLayer = animator.GetLayerIndex("hurt");
 
-        if(IsLocalPlayer)
+        cameraTransform = GetComponentInChildren<Camera>().transform;
+        if (IsLocalPlayer)
         {
             camZoomIn.SetActive(false);
             camZoomOut.SetActive(true);
+        }
+        else
+        {
+            camZoomIn.SetActive(false);
+            camZoomOut.SetActive(false);
+            cameraTransform.gameObject.GetComponent<Camera>().enabled = false;
+            cameraTransform.gameObject.GetComponent<AudioListener>().enabled = false;
         }
     }
 
@@ -120,13 +166,17 @@ public class PlayerController : NetworkBehaviour
     {
         if (IsClient && IsOwner)
         {
-            ClientInput();
+            
         }
-        ClientMoveAndRotate();
+        
         ClientVisuals();
 
         if (IsLocalPlayer)
         {
+            Look();
+            ClientInput();
+            ClientAim();
+            ClientMoveAndRotate();
             if (Input.GetKeyDown(KeyCode.E))
             {
                 playerAim = !playerAim;
@@ -140,8 +190,26 @@ public class PlayerController : NetworkBehaviour
                 playerHurt = !playerHurt;
             }
         }
+
         //SomeWait();
     }
+
+    private void ClientAim()
+    {
+        
+        if (oldAimLayerWeight != animator.GetLayerWeight(aimLayer))
+        {
+            if (playerAim == true)
+            {
+                UpdateClientAimLayerWeightServerRpc(0);
+            }
+            if (playerAim == false)
+            {
+                UpdateClientAimLayerWeightServerRpc(1);
+            }
+        }
+    }
+
 
     private void ClientInput()
     {
@@ -171,32 +239,32 @@ public class PlayerController : NetworkBehaviour
             UpdatePlayerAnimeStateServerRpc(PlayerAnimeState.Idle);
         }
 
-        if (playerAim == true)
-        {
-            PlayerNotAim();
-        }
-        else if (playerAim == false)
-        {
-            PlayerAim();
-        }
+        //if (playerAim == true)
+        //{
+        //    PlayerNotAim();
+        //}
+        //else if (playerAim == false)
+        //{
+        //    PlayerAim();
+        //}
 
-        if (playerHurt == true)
-        {
-            PlayerHurt();
-        }
-        else if (playerHurt == false)
-        {
-            PlayerNotHurt();
-        }
+        //if (playerHurt == true)
+        //{
+        //    PlayerHurt();
+        //}
+        //else if (playerHurt == false)
+        //{
+        //    PlayerNotHurt();
+        //}
 
-        if (playerThrow == true)
-        {
-            PlayerThrow();
-        }
-        else if (playerThrow == false)
-        {
-            PlayerNotThrow();
-        }
+        //if (playerThrow == true)
+        //{
+        //    PlayerThrow();
+        //}
+        //else if (playerThrow == false)
+        //{
+        //    PlayerNotThrow();
+        //}
     }
     private void ClientMoveAndRotate()
     {
@@ -212,6 +280,14 @@ public class PlayerController : NetworkBehaviour
         {
             transform.Rotate(networkRotationDirection.Value);
         }
+        if(networkAimLayerWeight.Value != 0)
+        {
+            PlayerAimServerRpc(0);
+        }
+        if (networkAimLayerWeight.Value == 0)
+        {
+            PlayerAimServerRpc(1);
+        }
     }
     private void ClientVisuals()
     {
@@ -226,10 +302,6 @@ public class PlayerController : NetworkBehaviour
         else if(networkPlayerState.Value == PlayerAnimeState.Idle)
         {
             animator.SetFloat("Walk", 0);
-        }
-        else if(networkPlayerState.Value == PlayerAnimeState.Hurt)
-        {
-            Debug.Log("yeah");
         }
     }
     
