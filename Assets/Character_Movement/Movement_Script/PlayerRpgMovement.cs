ad2c4bf4b91d087e01573511d64710589225c8fc
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2022-05-11 23:10:51 +0700

    Movement Package

---
+++ Assets/Character_Movement/Movement_Script/PlayerRpgMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+using Cinemachine;
+
+public class PlayerRpgMovement : NetworkBehaviour
+{
+    //private weapon weaponHeld      Inprogress**
+    [SerializeField]
+    private CharacterController controller;
+
+    [Range(0f, 10f)]
+    public float movementSpeed;
+    [Range(100f, 1000f)]
+    public float speedMultiplier;
+
+    [Range(0f, 1f)]
+    public float turnSmoothTime;
+    float turnSmoothVelocity;
+
+    [SerializeField]
+    private CinemachineFreeLook Vcam;
+    [SerializeField]
+    private Transform mainCam;
+    public bool canMove;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        mainCam = Camera.main.transform;
+        Vcam = GameObject.FindGameObjectWithTag("VirtualCam").GetComponent<CinemachineFreeLook>();
+
+        Vcam.Follow = this.gameObject.transform;
+        Vcam.LookAt = this.gameObject.transform;
+    }
+    void Update()
+    {
+        if (IsLocalPlayer)
+        {
+            if (!canMove) return;
+            Movement();
+        }
+    }
+    private void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+            rb.AddForce(moveDir.normalized * movementSpeed*speedMultiplier * Time.deltaTime);
+        }
+    }
+}
