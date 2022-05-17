97cdc6e7de9456ea7feadc39eae60bdd26197a85
MiyasakiSan <76218140+MiyasakiSan@users.noreply.github.com>
2022-05-17 12:30:03 +0700

    AddShader

---
+++ Assets/Shader/ShaderDisslove/Script/ModelDissloveScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelDissloveScript : MonoBehaviour
+{
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("TestScript"))
+        {
+            
+        }
+    }
+}
