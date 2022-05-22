2951ef3971327ba928ba4255680c0fd0c40968e7
MiyasakiSan <76218140+MiyasakiSan@users.noreply.github.com>
2022-05-22 18:41:39 +0700

    AlertCircle

--- Assets/Shader/ShaderSwordSlash/TestVFX.cs
+++ Assets/Shader/ShaderSwordSlash/TestVFX.cs
@@ -5,9 +5,11 @@ using UnityEngine.VFX;
 
 public class TestVFX : MonoBehaviour
 {
-    public VisualEffect visualEffect;
+    public VisualEffect[] visualEffect;
     public void OnPlayVFX()
     {
-        visualEffect.Play();
+        visualEffect[0].Play();
+        visualEffect[1].Play();
+        visualEffect[2].Play();
     }
 }
